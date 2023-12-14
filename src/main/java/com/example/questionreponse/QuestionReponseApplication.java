45d8ce5a5c79934ecbd6371dd16972a63c425883
mbaremedsalem <mbaremedsalemmbare@gmail.com>
2023-12-14 12:20:57 -0800

    fisrt commit

---
+++ src/main/java/com/example/questionreponse/QuestionReponseApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionreponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionReponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionReponseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/questionreponse/QuestionReponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionreponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionReponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
